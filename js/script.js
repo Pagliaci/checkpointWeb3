ac2e67fa4b5a629e1e30cc45046fd5ba0249d393
charlenefialho <fialhocharlene@gmail.com>
2023-05-16 17:47:38 -0300

    [ex2]evento lampada

--- js/script.js
+++ js/script.js
@@ -3,4 +3,15 @@ let num2 = document.querySelector('#num2')
 let executar = document.querySelector('#executar')
 
 
+let lampada = document.querySelector('#lampada')
+lampada.addEventListener('mouseover',acender)
+lampada.addEventListener('mouseout',apagar)
 
+
+function acender(){
+    lampada.src = 'img/lamp-acesa.jpg'
+}
+
+function apagar(){
+    lampada.src = 'img/lamp-apagada.jpg'
+}
