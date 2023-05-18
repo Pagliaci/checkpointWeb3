2e05a3a8f7c962f2b78dac0375b979ad9c0a331f
pagliacci <gianinisampaio1@gmail.com>
2023-05-18 08:01:39 -0300

    questao 3
    questao 3 finalizada

--- js/script.js
+++ js/script.js
@@ -1,6 +1,43 @@
-let num1 = document.querySelector('#num1')
-let num2 = document.querySelector('#num2')
-let executar = document.querySelector('#executar')
+
+function operacao(tipo) {
+    let num1 = Number (document.getElementById('num1'))
+    let num2 = Number (document.getElementById('num2'))
+    let result = document.getElementById('resultado')
+    if (tipo =='som'){
+        result.innerHTML = num1+num2
+    }
+    else if (tipo =='sub'){
+        result.innerHTML = num1-num2
+    }
+    
+    else if (tipo =='div'){
+        result.innerHTML = num1/num2
+    }
+    
+    else if (tipo =='mul'){
+        result.innerHTML = num1*num2
+    }
+    
+}
+
+function questoes(){
+    let nome = prompt('Qual o seu nome?')
+    let idade = prompt('Qual a sua idade?')
+    let curso = prompt('Qual o curso que você faz?')
+    document.getElementById('frase').innerHTML = `A pessoa se chama ${nome}, tem ${idade} anos e cursa ${curso}.`
+    
+}
+
+
+function cor(){
+    
+}
+
+
+
+
+
+
 
 
 
